650ce1cd526d1f391118eca89fb8437a839b0ed8
aryazad08 <aryazad08@gmail.com>
2026-02-14 22:51:13 +0530

    Remove persistent order notification banner from student layout

--- src/app/(student)/layout.tsx
+++ src/app/(student)/layout.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Header } from "@/components/Header"
-import { CurrentOrderBanner } from "@/components/CurrentOrderBanner"
 import { ThemeProvider } from "@/components/ThemeProvider"
 import { BottomNav } from "@/components/BottomNav"
 
@@ -16,7 +15,6 @@ export default function StudentLayout({
             <main className="min-h-[calc(100vh-4rem)] pb-20 sm:pb-8">
                 {children}
             </main>
-            <CurrentOrderBanner />
             <BottomNav />
         </ThemeProvider>
     )
